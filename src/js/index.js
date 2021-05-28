44fe7d8a6c8f398163f1ccd77f1944a7a00369b9
Белозерцев Артур <Turabot@i.ua>
2021-05-29 00:01:51 +0300

    refactors APP

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@ import {postRequest, URL} from "./helpers/request.js";
 import {setCookie} from "./helpers/cookieHelper.js";
 import {redirect} from "./helpers/general";
 import {renderError, renderText} from "./helpers/render.js";
-import {setLocalStorage} from "./helpers/localStorageOperations.js";
-import * as CryptoJS from "crypto-js";
+
 import axios from "axios";
+
 
 export const IndexInit = () => {
 
@@ -14,24 +14,8 @@ export const IndexInit = () => {
     const login = regForm.login;
     const password = regForm.password;
     const signButton = document.querySelector("#submit-auth");
-    const guestLink = document.querySelector("#guest");
     const errorText = document.querySelector(".error-text");
 
-    function _arrayBufferToBase64( buffer ) {
-        let binary = '';
-        let bytes = new Uint8Array( buffer );
-        let len = bytes.byteLength;
-        for (let i = 0; i < len; i++) {
-            binary += String.fromCharCode( bytes[ i ] );
-        }
-        return window.btoa( binary );
-    }
-
-    guestLink.addEventListener("click", (e) => {
-        e.preventDefault();
-        setLocalStorage("role", 'guest');
-        redirect("chat.html");
-    });
     signButton.addEventListener("click", (event) => {
         event.preventDefault();
         renderText(errorText, "");
@@ -39,28 +23,17 @@ export const IndexInit = () => {
             login: login.value,
             password: password.value,
         };
+
         const authURL = URL + "/users/auth";
         axios.post(authURL, bodyObject
-        ).then((data) => {
-            let buffer = data.data.split(".");
-            let publicToken = buffer[0];
-            let bytes  = CryptoJS.AES.decrypt(publicToken, "NotTheBestChoice");
+        ).then((response) => {
+            if (response.status === 200) {
 
-            let originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-            console.log(publicToken);
-            console.log(originalText);
-            //
-            // console.log(bytes);
-            // let privateToken = buffer[1];
-
-            // renderText(errorText, "");
-            console.log(data.status === 200);
-            if (data.status === 600) {
                 // if (data.body.role !== 'admin') {
                 //     data.body.role = 'user';
                 // }
-                // setCookie("token", data.body.token);
+                setCookie("nickName",response.headers.nickname);
+                setCookie("token", response.data);
                 // setLocalStorage("role", data.body.role);
                 redirect("chat.html");
             } else return renderError(errorText, "некоректный логин или пароль");
